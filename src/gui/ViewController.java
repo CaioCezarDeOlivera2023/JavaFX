078b8164f72f3f23ced2ed503cd42f547b0f8975
Caio Cezar <caio25m@gmail.com>
2025-01-29 09:34:19 -0300

    mostranto aleta na tele JavaFx

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,6 +1,8 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 
 public class ViewController {//aqui é classe responssavel por controlar a tela
@@ -10,7 +12,7 @@ public class ViewController {//aqui é classe responssavel por controlar a tela
 	
 	@FXML
 	public void onBtTestAction() {
-		System.out.println("Click");
+		Alerts.showAlert("Alert title", "Alert hearder", "Hello", AlertType.INFORMATION);
 	}
 	
 }
